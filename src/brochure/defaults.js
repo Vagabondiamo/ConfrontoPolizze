2449c029d96758d6b004d58d01c73946fc734db9
Vagabondiamo <zakariaessahili519@gmail.com>
2026-07-06 15:30:16 +0200

    Update

--- src/brochure/defaults.js
+++ src/brochure/defaults.js
@@ -42,7 +42,7 @@ export const EXAMPLE = {
       name: "Marco Bianchi",
       role: "Consulente Assicurativo",
     },
-    slogan: "Protegere | Crescere | Investire",
+    slogan: "Proteggere | Crescere | Investire",
   },
   policyB: {
     name: "Sicura Vita Premium",
